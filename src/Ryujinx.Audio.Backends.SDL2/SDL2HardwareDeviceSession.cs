5aba50ead1e3d533546d9de3f495ca063c6ce0da
swiftsword94 <swiftsword94@gmail.com>
2024-06-18 09:03:31 -0400

    Partial implementation of audio input for sdl2
    #4254

--- src/Ryujinx.Audio.Backends.SDL2/SDL2HardwareDeviceSession.cs
+++ src/Ryujinx.Audio.Backends.SDL2/SDL2HardwareDeviceSession.cs
@@ -7,7 +7,8 @@ using System;
 using System.Buffers;
 using System.Collections.Concurrent;
 using System.Threading;
-
+using System.Linq;
+using static Ryujinx.Audio.Integration.IHardwareDeviceDriver;
 using static SDL2.SDL;
 
 namespace Ryujinx.Audio.Backends.SDL2
@@ -19,7 +20,7 @@ namespace Ryujinx.Audio.Backends.SDL2
         private readonly DynamicRingBuffer _ringBuffer;
         private ulong _playedSampleCount;
         private readonly ManualResetEvent _updateRequiredEvent;
-        private uint _outputStream;
+        private uint _streamDeviceId;
         private bool _hasSetupError;
         private readonly SDL_AudioCallback _callbackDelegate;
         private readonly int _bytesPerFrame;
@@ -27,8 +28,9 @@ namespace Ryujinx.Audio.Backends.SDL2
         private bool _started;
         private float _volume;
         private readonly ushort _nativeSampleFormat;
+        private readonly Direction _direction;
 
-        public SDL2HardwareDeviceSession(SDL2HardwareDeviceDriver driver, IVirtualMemoryManager memoryManager, SampleFormat requestedSampleFormat, uint requestedSampleRate, uint requestedChannelCount) : base(memoryManager, requestedSampleFormat, requestedSampleRate, requestedChannelCount)
+        public SDL2HardwareDeviceSession(SDL2HardwareDeviceDriver driver, IVirtualMemoryManager memoryManager, SampleFormat requestedSampleFormat, uint requestedSampleRate, uint requestedChannelCount, Direction direction ) : base(memoryManager, requestedSampleFormat, requestedSampleRate, requestedChannelCount)
         {
             _driver = driver;
             _updateRequiredEvent = _driver.GetUpdateRequiredEvent();
@@ -40,32 +42,33 @@ namespace Ryujinx.Audio.Backends.SDL2
             _sampleCount = uint.MaxValue;
             _started = false;
             _volume = 1f;
+            _direction = direction;
         }
 
         private void EnsureAudioStreamSetup(AudioBuffer buffer)
         {
             uint bufferSampleCount = (uint)GetSampleCount(buffer);
-            bool needAudioSetup = (_outputStream == 0 && !_hasSetupError) ||
+            bool needAudioSetup = (_streamDeviceId == 0 && !_hasSetupError) ||
                 (bufferSampleCount >= Constants.TargetSampleCount && bufferSampleCount < _sampleCount);
 
             if (needAudioSetup)
             {
                 _sampleCount = Math.Max(Constants.TargetSampleCount, bufferSampleCount);
 
-                uint newOutputStream = SDL2HardwareDeviceDriver.OpenStream(RequestedSampleFormat, RequestedSampleRate, RequestedChannelCount, _sampleCount, _callbackDelegate);
+                uint newStream = SDL2HardwareDeviceDriver.OpenStream(RequestedSampleFormat, _direction, RequestedSampleRate, RequestedChannelCount, _sampleCount, _callbackDelegate);
 
-                _hasSetupError = newOutputStream == 0;
+                _hasSetupError = newStream == 0;
 
                 if (!_hasSetupError)
                 {
-                    if (_outputStream != 0)
+                    if (_streamDeviceId != 0)
                     {
-                        SDL_CloseAudioDevice(_outputStream);
+                        SDL_CloseAudioDevice(_streamDeviceId);
                     }
 
-                    _outputStream = newOutputStream;
+                    _streamDeviceId = newStream;
 
-                    SDL_PauseAudioDevice(_outputStream, _started ? 0 : 1);
+                    SDL_PauseAudioDevice(_streamDeviceId, _started ? 0 : 1);
 
                     Logger.Info?.Print(LogClass.Audio, $"New audio stream setup with a target sample count of {_sampleCount}");
                 }
@@ -88,47 +91,142 @@ namespace Ryujinx.Audio.Backends.SDL2
 
                 return;
             }
+            
+            ulong availaibleSampleCount;
+            bool needUpdate = false;
 
             using IMemoryOwner<byte> samplesOwner = ByteMemoryPool.Rent(frameCount * _bytesPerFrame);
 
             Span<byte> samples = samplesOwner.Memory.Span;
 
-            _ringBuffer.Read(samples, 0, samples.Length);
-
-            fixed (byte* p = samples)
+            if (_direction == Direction.Output)
             {
-                IntPtr pStreamSrc = (IntPtr)p;
+                _ringBuffer.Read(samples, 0, samples.Length);
 
-                // Zero the dest buffer
-                streamSpan.Clear();
-
-                // Apply volume to written data
-                SDL_MixAudioFormat(stream, pStreamSrc, _nativeSampleFormat, (uint)samples.Length, (int)(_driver.Volume * _volume * SDL_MIX_MAXVOLUME));
-            }
-
-            ulong sampleCount = GetSampleCount(samples.Length);
-
-            ulong availaibleSampleCount = sampleCount;
-
-            bool needUpdate = false;
-
-            while (availaibleSampleCount > 0 && _queuedBuffers.TryPeek(out SDL2AudioBuffer driverBuffer))
-            {
-                ulong sampleStillNeeded = driverBuffer.SampleCount - Interlocked.Read(ref driverBuffer.SamplePlayed);
-                ulong playedAudioBufferSampleCount = Math.Min(sampleStillNeeded, availaibleSampleCount);
-
-                ulong currentSamplePlayed = Interlocked.Add(ref driverBuffer.SamplePlayed, playedAudioBufferSampleCount);
-                availaibleSampleCount -= playedAudioBufferSampleCount;
-
-                if (currentSamplePlayed == driverBuffer.SampleCount)
+                fixed (byte* p = samples)
                 {
-                    _queuedBuffers.TryDequeue(out _);
+                    IntPtr pStreamSrc = (IntPtr)p;
 
-                    needUpdate = true;
+                    // Zero the dest buffer
+                    streamSpan.Clear();
+                    // Apply volume to written data
+                    SDL_MixAudioFormat(stream, pStreamSrc, _nativeSampleFormat, (uint)samples.Length, (int)(_driver.Volume * _volume * SDL_MIX_MAXVOLUME));
+                }
+                ulong sampleCount = GetSampleCount(samples.Length);
+                availaibleSampleCount = sampleCount;
+
+                while (availaibleSampleCount > 0 && _queuedBuffers.TryPeek(out SDL2AudioBuffer driverBuffer))
+                {
+                    ulong sampleStillNeeded = driverBuffer.SampleCount - Interlocked.Read(ref driverBuffer.SamplePlayed);
+                    ulong playedAudioBufferSampleCount = Math.Min(sampleStillNeeded, availaibleSampleCount);
+
+                    ulong currentSamplePlayed = Interlocked.Add(ref driverBuffer.SamplePlayed, playedAudioBufferSampleCount);
+                    availaibleSampleCount -= playedAudioBufferSampleCount;
+
+                    if (currentSamplePlayed == driverBuffer.SampleCount)
+                    {
+                        _queuedBuffers.TryDequeue(out _);
+
+                        needUpdate = true;
+                    }
+
+                    Interlocked.Add(ref _playedSampleCount, playedAudioBufferSampleCount);
+                }
+            }
+            else
+            {
+
+                _ringBuffer.Read(samples, 0, samples.Length);
+                // https://gist.github.com/andraantariksa/f5e6d848364b11a425625ec7fbbfc187
+                // make a set of AudioBuffers by copying data from stream
+
+                var frameLengthInBytes = frameCount * _bytesPerFrame;
+
+                //byte[] microphoneByteArr = new byte[frameLengthInBytes];
+                var microphoneData = new Span<byte>((void*)stream, frameLengthInBytes);
+
+                //microphoneData.CopyTo(microphoneByteArr);
+
+                //GetSampleCount(microphoneData.Length);
+
+                //microphoneData.CopyTo(samples);
+
+                for (int i = 0; i < _queuedBuffers.Count && i * frameCount < frameLengthInBytes; i++)
+                {
+                    Span<byte> microphoneAudioSpan = microphoneData.Slice(i * frameCount, frameCount);
+                    var data = microphoneAudioSpan.ToArray();
+
+                    // don't need to queue buffers, just write data into 4 currently existing buffers  in _queuedBuffers
+                    var buf = _queuedBuffers.ElementAt(i);
+
+                    Span<byte> buffer = new((void*)(IntPtr)buf.DriverIdentifier, (int)buf.SampleCount);
+
+                    //_ringBuffer.Write(microphoneAudioSpan, 0, frameCount);
+
+                    microphoneAudioSpan.CopyTo(buffer);
+
+                    fixed (byte* p = buffer)
+                    {
+                        //    //IntPtr n = (IntPtr.)buf.DriverIdentifier;
+                        //    //n[0] = data;
+                        //    //var buffer = new AudioBuffer()
+                        //    //{
+                        //    //    Data = data,
+                        //    //    DataSize = (ulong)data.Length,
+                        //    //    DataPointer = (ulong)(IntPtr)p,
+                        //    //};
+                        //    //QueueBuffer(buffer);
+
+                        //    // use this section to write into the buffers
+
+
+                        //    //buf.SampleCount = GetSampleCount(data.Length);
+                        //    //buf.DriverIdentifier = (ulong)(IntPtr)p;
+                        //    //buf.SamplePlayed = 0;
+                    }
                 }
 
-                Interlocked.Add(ref _playedSampleCount, playedAudioBufferSampleCount);
+
+                // once buffer queued update the played sample count
+                ulong sampleCount = GetSampleCount(frameCount * _bytesPerFrame);
+                availaibleSampleCount = sampleCount;
+
+                //Interlocked.Add(ref _sampleCount, (uint)availaibleSampleCount);
+
+
+
+                //Interlocked.Increment(ref _playedSampleCount);
+
+                //needUpdate = true;
+                //_queuedBuffers.TryDequeue(out _); <---- this actually submits the audio to the game
+
+
+                // use interlocked add to increment playedSampleCount? or do i decrease or ignore
+
+                // format that its being captured or sent is incorrect. data looks wierd 
+
+                while (availaibleSampleCount > 0 && _queuedBuffers.TryPeek(out SDL2AudioBuffer driverBuffer))
+                {
+                    ulong sampleStillNeeded = driverBuffer.SampleCount - Interlocked.Read(ref driverBuffer.SamplePlayed);
+                    ulong playedAudioBufferSampleCount = Math.Min(sampleStillNeeded, availaibleSampleCount);
+
+                    ulong currentSamplePlayed = Interlocked.Add(ref driverBuffer.SamplePlayed, playedAudioBufferSampleCount);
+                    availaibleSampleCount -= playedAudioBufferSampleCount;
+
+                    if (currentSamplePlayed == driverBuffer.SampleCount)
+                    {
+                        _queuedBuffers.TryDequeue(out _);
+
+                        needUpdate = true;
+                    }
+
+                    Interlocked.Add(ref _playedSampleCount, playedAudioBufferSampleCount);
+                }
             }
+
+            
+
+
 
             // Notify the output if needed.
             if (needUpdate)
@@ -153,7 +251,7 @@ namespace Ryujinx.Audio.Backends.SDL2
         {
             EnsureAudioStreamSetup(buffer);
 
-            if (_outputStream != 0)
+            if (_streamDeviceId != 0)
             {
                 SDL2AudioBuffer driverBuffer = new(buffer.DataPointer, GetSampleCount(buffer));
 
@@ -178,9 +276,9 @@ namespace Ryujinx.Audio.Backends.SDL2
         {
             if (!_started)
             {
-                if (_outputStream != 0)
+                if (_streamDeviceId != 0)
                 {
-                    SDL_PauseAudioDevice(_outputStream, 0);
+                    SDL_PauseAudioDevice(_streamDeviceId, 0);
                 }
 
                 _started = true;
@@ -191,9 +289,9 @@ namespace Ryujinx.Audio.Backends.SDL2
         {
             if (_started)
             {
-                if (_outputStream != 0)
+                if (_streamDeviceId != 0)
                 {
-                    SDL_PauseAudioDevice(_outputStream, 1);
+                    SDL_PauseAudioDevice(_streamDeviceId, 1);
                 }
 
                 _started = false;
@@ -219,9 +317,9 @@ namespace Ryujinx.Audio.Backends.SDL2
                 PrepareToClose();
                 Stop();
 
-                if (_outputStream != 0)
+                if (_streamDeviceId != 0)
                 {
-                    SDL_CloseAudioDevice(_outputStream);
+                    SDL_CloseAudioDevice(_streamDeviceId);
                 }
             }
         }
